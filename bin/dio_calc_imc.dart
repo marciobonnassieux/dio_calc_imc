36a9c547578244d89050bb8998181de38fb49e3f
Marcio Fernandes <marciobonnassieux@gmail.com>
2025-03-28 00:47:39 -0300

    initial

---
+++ bin/dio_calc_imc.dart
@@ -0,0 +1,5 @@
+import 'package:dio_calc_imc/dio_calc_imc.dart' as dio_calc_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dio_calc_imc.calculate()}!');
+}
---
+++ test/dio_calc_imc_test.dart
@@ -0,0 +1,8 @@
+import 'package:dio_calc_imc/dio_calc_imc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
